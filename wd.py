3fdb528e59eee4ee4389d059df06329e4fe32d98
Jay <jay0x55@protonmail.com>
2022-02-20 08:14:57 -0500

    updated

--- wd.py
+++ wd.py
@@ -1,14 +1,32 @@
 import os
 
+
 def yup(damn, c):
     for x in c:
+        if x[0].startswith('\n'):
+            x[0] = x[0][1:]
         damn = damn.replace(x[0], x[1])
     return damn
+
+
+def replace(dr, par):
+    for i in os.listdir(dr):
+        path = dr + "/" + i
+        if os.path.isdir(path):
+            replace(path, par)
+            continue
+        if path.split(".")[-1] != "html":
+            continue
+        with open(path, "r", encoding="utf-8") as content:
+            href = yup(content.read(), par)
+            content.close()
+        with open(path, "w", encoding="utf-8") as data:
+            data.write(href)
+
 
 def run():
     conf = input("config >>> ")
     dr = input("Folder: ")
-    par = []
     print("[+] running ....")
 
     with open(conf, 'r', encoding="utf-8") as cnf:
@@ -16,13 +34,7 @@ def run():
         if config[-1] == "\n":
             config.pop()
         par = [x.split("&") for x in config]
-    for i in os.listdir(dr):
-        with open(dr + "/" + i, "r", encoding="utf-8") as content:
-            href = yup(content.read(), par)
-            content.close()
-        with open(dr + "/" + i, "w", encoding="utf-8") as data:
-            data.write(href)
-
+        replace(dr, par)
     print("\tOK")
     input("\n\npress any key to exit")
 
